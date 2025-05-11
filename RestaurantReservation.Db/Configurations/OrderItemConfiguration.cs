eb45b11b53fc5c52325a5a8df90dc1517b2316ab
jafar fuad <jata1518@gmail.com>
2025-05-11 23:17:48 +0300

    Added OrderItemConfiguration class for entity configuration in RestaurantReservation.Db

---
+++ RestaurantReservation.Db/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.Property(i => i.OrderItemId).HasColumnName("order_item_id");
+            builder.Property(i => i.OrderId).HasColumnName("order_id");
+            builder.Property(i => i.ItemId).HasColumnName("item_id");
+            builder.Property(i => i.Quantity).HasColumnName("quantity");
+
+            builder.HasOne(i => i.Item)
+              .WithMany(o => o.OrderItems)
+              .HasForeignKey(i => i.ItemId)
+              .OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.HasOne(o => o.Order)
+              .WithMany(oi => oi.OrderItems)
+              .HasForeignKey(o => o.OrderId)
+              .OnDelete(DeleteBehavior.ClientSetNull);
+        }
+    }
+
+}
