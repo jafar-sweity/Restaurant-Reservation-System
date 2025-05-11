6d5c014d088517cfd8fd4c7d151a0f565ff2da92
jafar fuad <jata1518@gmail.com>
2025-05-11 23:22:43 +0300

    Updated RestaurantReservationDbContext to include DbSet properties and apply configurations

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -1,19 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Configurations;
 using RestaurantReservation.Db.Models.Entities;
 
 namespace RestaurantReservation.Db
 {
     public class RestaurantReservationDbContext : DbContext
     {
-
-        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Customer> Customers { get; set; }
         public DbSet<Employee> Employees { get; set; }
-        public DbSet<Table> Tables { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Table> Tables { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // i will do it later 
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CustomerConfiguration).Assembly);
+        }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=LAPTOP-49OTT63M\\SQLEXPRESS; Database=RestaurantReservationCore;Integrated Security = SSPI; TrustServerCertificate = True;");
+            }
         }
     }
 }
