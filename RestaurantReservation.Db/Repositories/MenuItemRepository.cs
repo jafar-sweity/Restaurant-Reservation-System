2af14a2074869e72811755e4a55c03f8b0309d4d
jafar fuad <jata1518@gmail.com>
2025-05-12 18:49:49 +0300

    Add MenuItemRepository for CRUD operations and implement customer management in Program

---
+++ RestaurantReservation.Db/Repositories/MenuItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using RestaurantReservation.Db.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.Repositories
+{
+    public class MenuItemRepository : Repository<MenuItem>
+    {
+        public MenuItemRepository(RestaurantReservationDbContext context) : base(context)
+        {
+        }
+    }
+}
