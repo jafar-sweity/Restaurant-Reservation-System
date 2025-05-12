ef8865464668f9739e3942522c378051ed49a522
jafar fuad <jata1518@gmail.com>
2025-05-12 18:58:25 +0300

    Add GetReservationsByCustomer method to ReservationRepository for retrieving reservations by customer ID

---
+++ RestaurantReservation.Db/Repositories/ReservationRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.Repositories
+{
+    public class ReservationRepository : Repository<Reservation>
+    {
+        private readonly RestaurantReservationDbContext _context;
+
+
+        public ReservationRepository(RestaurantReservationDbContext context) : base(context) {
+            _context = context;
+        }
+
+        public async Task<List<Reservation>> GetReservationsByCustomerIdAsync(int customerId)
+        {
+            return await _context.Reservations.Where(r => r.customerId == customerId).ToListAsync();
+        }
+    }
+}
