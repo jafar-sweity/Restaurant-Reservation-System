9c22c69a9252bb127777196fba08e457a643bbf0
jafar fuad <jata1518@gmail.com>
2025-05-12 21:00:07 +0300

    Add ListOrderedMenuItems method to MenuItemRepository for listing menu items ordered by reservation ID

--- RestaurantReservation.Db/Repositories/OrderItemRepository.cs
+++ RestaurantReservation.Db/Repositories/OrderItemRepository.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantReservation.Db.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Interfaces;
 using RestaurantReservation.Db.Models.Entities;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,21 @@ using System.Threading.Tasks;
 
 namespace RestaurantReservation.Db.Repositories
 {
+
     public class OrderItemRepository : Repository<OrderItem>, IRepository<OrderItem>
     {
+        private readonly RestaurantReservationDbContext _context;
+
         public OrderItemRepository(RestaurantReservationDbContext context) : base(context) { }
+
+        public async Task<List<MenuItem>> ListOrderedMenuItems(int ReservationId)
+        {
+            return await _context.OrderItems
+                                 .Include(oi => oi.Item)
+                                 .Include(oi => oi.Order)
+                                 .Where(oi => oi.Order.ReservationId == ReservationId).Select(i => i.Item).ToListAsync();
+        }
+
 
     }
 }
