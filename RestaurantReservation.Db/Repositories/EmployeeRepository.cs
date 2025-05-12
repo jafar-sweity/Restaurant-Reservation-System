ee6c18191cd20f1998064403dade8894124ced88
jafar fuad <jata1518@gmail.com>
2025-05-12 18:49:19 +0300

    Add EmployeeRepository for CRUD operations and implement customer management in Program

---
+++ RestaurantReservation.Db/Repositories/EmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.Repositories
+{
+    public class EmployeeRepository : Repository<Employee>
+    {
+        private readonly RestaurantReservationDbContext _context;
+
+        public EmployeeRepository(RestaurantReservationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public Task<List<Employee>> ListManagersAsync()
+        { 
+            return _context.Employees.Where(e=>e.Position == Models.Enum.EmployeePosition.Manager)
+                .ToListAsync();
+        }
+    }
+}
