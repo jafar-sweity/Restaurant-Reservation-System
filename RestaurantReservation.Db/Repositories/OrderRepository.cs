a544c41dfc12834b0d73afc4638203fa71269f3c
jafar fuad <jata1518@gmail.com>
2025-05-12 18:50:11 +0300

    Add OrderRepository for CRUD operations and implement customer management in Program

---
+++ RestaurantReservation.Db/Repositories/OrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantReservation.Db.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.Repositories
+{
+    public class OrderRepository : Repository<Order>
+    {
+        public OrderRepository(RestaurantReservationDbContext context) : base(context) { }
+
+    }
+}
