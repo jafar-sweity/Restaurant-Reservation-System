1e313cf3427bcb0b9bcf7781e95f842f4c03dbfe
jafar fuad <jata1518@gmail.com>
2025-05-12 19:36:33 +0300

    Add ListOrdersAndMenuItems method to OrderRepository to retrieve orders and associated menu items by reservation ID

--- RestaurantReservation.Db/Repositories/OrderRepository.cs
+++ RestaurantReservation.Db/Repositories/OrderRepository.cs
@@ -1,4 +1,6 @@
-﻿using RestaurantReservation.Db.Models.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using RestaurantReservation.Db.Models.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +11,17 @@ namespace RestaurantReservation.Db.Repositories
 {
     public class OrderRepository : Repository<Order>
     {
+        private readonly RestaurantReservationDbContext _context;
+
         public OrderRepository(RestaurantReservationDbContext context) : base(context) { }
 
+        public async Task<List<Order>> ListOrdersAndMenuItems(int ReservationId)
+        {
+            return  await _context.Orders
+                                .Include(o => o.OrderItems)
+                                .ThenInclude(oi => oi.Item)
+                                .Where(o => o.ReservationId == ReservationId).ToListAsync();
+
+        }
     }
 }
