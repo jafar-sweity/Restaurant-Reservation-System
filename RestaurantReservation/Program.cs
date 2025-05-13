236ecb2ac59bdcd52dd389c5c8e670d32478ffd0
jafar fuad <jata1518@gmail.com>
2025-05-13 23:05:20 +0300

    Removed unnecessary using  & Extra lines

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using RestaurantReservation.Db;
 
 namespace RestaurantReservation
@@ -15,18 +11,17 @@ namespace RestaurantReservation
         static async Task Main(string[] args)
         {
             var host = Host.CreateDefaultBuilder(args)
-       .ConfigureAppConfiguration((hostingContext, config) =>
-        {
-           config.AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true);
-       })
-       .ConfigureServices((context, services) =>
-       {
-           services.AddDbContext<RestaurantReservationDbContext>(options =>
-               options.UseSqlServer(context.Configuration.GetConnectionString("RestaurantReservationDb")));
-
-           services.AddTransient<TesterClass>();
-       })
-       .Build();
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddDbContext<RestaurantReservationDbContext>(options =>
+                        options.UseSqlServer(context.Configuration.GetConnectionString("RestaurantReservationDb")));
+                    services.AddTransient<TesterClass>();
+                })
+                .Build();
 
             using (var scope = host.Services.CreateScope())
             {
