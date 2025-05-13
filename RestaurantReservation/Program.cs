0877ee352f05117235b16ff0cf6b70624f1c6ae4
jafar fuad <jata1518@gmail.com>
2025-05-13 22:51:09 +0300

    Read connection string from appsettings.json in OnConfiguring method

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,7 +1,12 @@
-﻿using RestaurantReservation.Db;
-using RestaurantReservation.Db.Models.Entities;
-using RestaurantReservation.Db.Models.Enum;
-using RestaurantReservation.Db.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using RestaurantReservation.Db;
 
 namespace RestaurantReservation
 {
@@ -9,9 +14,26 @@ namespace RestaurantReservation
     {
         static async Task Main(string[] args)
         {
-            var context = new RestaurantReservationDbContext();
-            var reservationTester = new TesterClass(context);
-            await reservationTester.Test();
+            var host = Host.CreateDefaultBuilder(args)
+       .ConfigureAppConfiguration((hostingContext, config) =>
+        {
+           config.AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true);
+       })
+       .ConfigureServices((context, services) =>
+       {
+           services.AddDbContext<RestaurantReservationDbContext>(options =>
+               options.UseSqlServer(context.Configuration.GetConnectionString("RestaurantReservationDb")));
+
+           services.AddTransient<TesterClass>();
+       })
+       .Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var reservationTester = services.GetRequiredService<TesterClass>();
+                await reservationTester.Test();
+            }
         }
     }
 }
