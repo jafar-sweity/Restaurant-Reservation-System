eb3af233eb5aff98e0078b7922321657e3d7982d
jafar fuad <jata1518@gmail.com>
2025-05-21 22:32:00 +0300

    Add mapping between Employee and EmployeeUpdateDto with reverse mapping in AutoMapper configuration

---
+++ RestaurantReservation.API/Profiles/EmployeeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.Employees;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class EmployeeProfile:Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<Employee, EmployeeDto>().ReverseMap();
+        }
+    }
+}
