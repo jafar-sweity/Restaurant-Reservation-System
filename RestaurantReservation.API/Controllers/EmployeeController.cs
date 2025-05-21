b8ccbcd86d282da4389cd1ac20c971c1b5b75d11
jafar fuad <jata1518@gmail.com>
2025-05-21 22:37:40 +0300

    Add EmployeesController with GetEmployees endpoint to retrieve all employees and return EmployeeDto list & Add GetEmployee endpoint to retrieve an employee by ID

---
+++ RestaurantReservation.API/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.Employees;
+using RestaurantReservation.Db.Interfaces;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IRepository<Employee> _repository;
+        private readonly IMapper _mapper;
+
+        public EmployeeController(IRepository<Employee> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmployeeDto>>> GetAllEmployees()
+        {
+            var employees = await _repository.GetAllAsync();
+            if (employees == null || !employees.Any())
+            {
+                return NotFound("No employees found.");
+            }
+            var employeeDtos = _mapper.Map<IEnumerable<EmployeeDto>>(employees);
+            return Ok(employeeDtos);
+        }
+
+        [HttpGet("{id:int}", Name = "GetEmployee")]
+        public async Task<ActionResult<EmployeeDto>> GetEmployee(int id)
+        {
+            var employee = await _repository.GetByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound($"Employee with ID {id} not found.");
+            }
+            var employeeDto = _mapper.Map<EmployeeDto>(employee);
+            return Ok(employeeDto);
+        }
+
+    }
+}
