8e6cef86293d7866a2191b24f83dc82b8932e74c
jafar fuad <jata1518@gmail.com>
2025-05-22 21:17:48 +0300

    Implement OrderItemsController with endpoint to retrieve order items by order ID

---
+++ RestaurantReservation.API/Controllers/OrderItemController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.OrderItem;
+using RestaurantReservation.Db.Interfaces;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Controllers
+{
+    public class OrderItemController : Controller
+    {
+        private readonly IRepository<OrderItem> _repository;
+        private readonly IMapper _mapper;
+
+        public OrderItemController(IRepository<OrderItem> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderItemDto>>> GetAllOrderItems()
+        {
+            var orderItems = await _repository.GetAllAsync();
+            if (orderItems == null || !orderItems.Any())
+            {
+                return NotFound("No order items found.");
+            }
+            var orderItemDtos = _mapper.Map<IEnumerable<OrderItemDto>>(orderItems);
+            return Ok(orderItemDtos);
+        }
+    }
+}
