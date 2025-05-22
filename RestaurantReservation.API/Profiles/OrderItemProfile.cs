70c554d3cb60c67ad7bfd5c8b8d5c6785374bcc4
jafar fuad <jata1518@gmail.com>
2025-05-22 20:48:02 +0300

    Add OrderItemProfile for mapping between OrderItem and OrderItemDto

---
+++ RestaurantReservation.API/Profiles/OrderItemProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.OrderItem;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class OrderItemProfile: Profile
+    {
+        public OrderItemProfile()
+        {
+            CreateMap<OrderItem, OrderItemDto>();
+        }
+    }
+}
