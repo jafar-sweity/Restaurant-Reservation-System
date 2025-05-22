59b6d978251903f17f66371a8b210f19b7587f4b
jafar fuad <jata1518@gmail.com>
2025-05-23 00:58:18 +0300

    Add ReservationsController with GetReservations method for retrieving all reservations

---
+++ RestaurantReservation.API/Controllers/ReservationController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.Reservation;
+using RestaurantReservation.Db.Interfaces;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : Controller
+    {
+        private readonly IRepository<Reservation> _repository;
+        private readonly IRepository<Restaurant> _reservationRepository;
+        private readonly IMapper _mapper;
+
+        public ReservationController(IRepository<Reservation> repository, IMapper mapper, IRepository<Restaurant> reservationRepository)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _reservationRepository = reservationRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ReservationDto>>> GetReservations()
+        {
+            var reservations = await _repository.GetAllAsync();
+            if (reservations == null || !reservations.Any())
+            {
+                return NotFound("No reservations found.");
+            }
+            var reservationDtos = _mapper.Map<IEnumerable<ReservationDto>>(reservations);
+            return Ok(reservationDtos);
+        }
+    }
+}
