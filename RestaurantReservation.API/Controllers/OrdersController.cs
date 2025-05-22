fa3d465349f183ddf508931f6ec4b4dab0ea72c8
jafar fuad <jata1518@gmail.com>
2025-05-23 00:36:08 +0300

    Implement OrdersController with GetOrders for fetching all orders with AutoMapper

---
+++ RestaurantReservation.API/Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.Order;
+using RestaurantReservation.Db.Interfaces;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IRepository<Order> _repository;
+        private readonly IMapper _mapper;
+
+        public OrdersController(IRepository<Order> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderDto>>> GetAllOrders()
+        {
+            var orders = await _repository.GetAllAsync();
+            if (orders == null || !orders.Any())
+            {
+                return NotFound("No orders found.");
+            }
+            var orderDtos = _mapper.Map<IEnumerable<OrderDto>>(orders);
+            return Ok(orderDtos);
+        }
+    }
+}
