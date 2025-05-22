e7bc9c8435758da131035393a63aac757806fe88
jafar fuad <jata1518@gmail.com>
2025-05-23 00:34:37 +0300

    Add AutoMapper profile for Order to OrderDto mapping

---
+++ RestaurantReservation.API/Profiles/OrderProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.Order;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class OrderProfile: Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, OrderDto>();
+
+        }
+    }
+}
