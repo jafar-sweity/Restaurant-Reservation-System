5d44fb3bf2ddee05b4b0b0c72f217e2580788eb6
jafar fuad <jata1518@gmail.com>
2025-05-23 01:13:30 +0300

    Add update and partial update methods for restaurants

--- RestaurantReservation.API/Controllers/RestaurantController.cs
+++ RestaurantReservation.API/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using RestaurantReservation.API.Models.Restaurant;
 using RestaurantReservation.Db.Interfaces;
@@ -55,6 +56,48 @@ namespace RestaurantReservation.API.Controllers
             var restaurantDto = _mapper.Map<RestaurantDto>(createdRestaurant);
 
             return CreatedAtRoute("GetRestaurant", new { id = restaurantDto.RestaurantId }, restaurantDto);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateRestaurant(int id, RestaurantUpdateDto restaurantUpdateDto)
+        {
+            if (restaurantUpdateDto == null)
+            {
+                return BadRequest("Restaurant data is null.");
+            }
+            var existingRestaurant = await _repository.GetByIdAsync(id);
+            if (existingRestaurant == null)
+            {
+                return NotFound($"Restaurant with ID {id} not found.");
+            }
+            var restaurant = _mapper.Map<Restaurant>(restaurantUpdateDto);
+            restaurant.RestaurantId = id;
+            await _repository.UpdateAsync(restaurant);
+            return NoContent();
+        }
+
+        [HttpPatch("{id:int}")]
+        public async Task<IActionResult> PartiallyUpdateRestaurant(int id, JsonPatchDocument<RestaurantUpdateDto> patchDocument)
+        {
+            if (patchDocument == null)
+            {
+                return BadRequest("Invalid patch document.");
+            }
+            var existingRestaurant = await _repository.GetByIdAsync(id);
+            if (existingRestaurant == null)
+            {
+                return NotFound($"Restaurant with ID {id} not found.");
+            }
+            var restaurantToPatch = _mapper.Map<RestaurantUpdateDto>(existingRestaurant);
+            patchDocument.ApplyTo(restaurantToPatch, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var restaurant = _mapper.Map<Restaurant>(restaurantToPatch);
+            restaurant.RestaurantId = id;
+            await _repository.UpdateAsync(restaurant);
+            return NoContent();
         }
     }
 }
