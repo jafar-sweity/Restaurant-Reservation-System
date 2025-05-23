38d989f88c5ef3ad59170e840085ffb3a8c9df5d
jafar fuad <jata1518@gmail.com>
2025-05-23 15:03:15 +0300

    Add TableController and Implement methods to retrieve all tables and a specific table by ID

---
+++ RestaurantReservation.API/Controllers/TableController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.Table;
+using RestaurantReservation.Db.Interfaces;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TableController : Controller
+    { 
+        private readonly IRepository<Table> _repository;
+        private readonly IMapper _mapper;
+        private readonly IRepository<Restaurant> _restaurantRepository;
+
+        public TableController(IRepository<Table> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TableDto>> GetAll()
+        {
+            var tables = await _repository.GetAllAsync();
+            var tableDtos = _mapper.Map<IEnumerable<TableDto>>(tables);
+            return Ok(tableDtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TableDto>> GetById(int id)
+        {
+            var table = await _repository.GetByIdAsync(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+            var tableDto = _mapper.Map<TableDto>(table);
+            return Ok(tableDto);
+        }
+
+    }
+}
